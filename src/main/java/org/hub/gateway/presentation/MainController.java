ffb8a05d11d016d4f6b7bda078296422db353fb1
HeeHo Lim <121926482+hiho1010@users.noreply.github.com>
2025-11-24 17:44:12 +0900

    [Feat] Gateway에 Swagger 통합

---
+++ src/main/java/org/hub/gateway/presentation/MainController.java
@@ -0,0 +1,13 @@
+package org.hub.gateway.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.result.view.RedirectView;
+
+@Controller
+public class MainController {
+    @GetMapping
+    public RedirectView index() {
+        return new RedirectView("/api-docs.html");
+    }
+}
